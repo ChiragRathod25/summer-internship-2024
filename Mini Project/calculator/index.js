f2990c39e6a1bea4e78615710a8811755a8deda7
Chirag Rathod <chiragrathod9987@gmail.com>
2024-06-14 22:08:01 +0530

    calculator using JS

--- Mini Project/calculator/index.js
+++ Mini Project/calculator/index.js
@@ -5,6 +5,7 @@ let previousexpression;
 let expressionAnswer;
 let answer;
 let expressionHistory = new Map();
+let historyContainer = document.querySelector(".historyContainerTable");
 let expressionField = document.querySelector(".expression");
 let expressionAnswerField = document.querySelector(".answer");
 
@@ -16,23 +17,24 @@ function resetExpression() {
   expressionField.innerHTML = 0;
   expressionAnswerField.innerHTML = 0;
 }
+
 function updateExpressionField() {
   try {
-    if(!expressionHistory[expression]){
-        answer = eval(expression);
-        console.log(typeof answer);
-        if (typeof answer === "number") {
-            expressionAnswerField.innerHTML = answer.toFixed(2);
-            console.log(expressionAnswerField.innerHTML);
-            expressionHistory[expression] = answer;
-            console.log(expressionHistory)
-            expression=undefined
-        }
-    }
-    else{
-        answer=expressionHistory[expression]
+    if (!expressionHistory[expression]) {
+      answer = eval(expression);
+      console.log(typeof answer);
+      if (typeof answer === "number") {
         expressionAnswerField.innerHTML = answer.toFixed(2);
-        expression=undefined
+        console.log(expressionAnswerField.innerHTML);
+        expressionHistory[expression] = answer;
+        console.log(expressionHistory);
+        updateHistory()
+        expression = undefined;
+      }
+    } else {
+      answer = expressionHistory[expression];
+      expressionAnswerField.innerHTML = answer.toFixed(2);
+      expression = undefined;
     }
   } catch (error) {
     expressionAnswerField.innerHTML = "Enter valid expression";
@@ -42,20 +44,28 @@ function updateExpressionField() {
   console.log(answer);
 }
 
-function getHistory() {}
-
 for (const buttons of btns) {
   buttons.addEventListener("click", function (e) {
     const value = buttons.getAttribute("value");
     // console.log(e)
     console.log(value);
     if (value === "=") {
-        // console.log(expression)
-    
+      // console.log(expression)
+
       updateExpressionField();
     } else if (value === "reset") {
       resetExpression();
       console.log(`Expression : ${expression}`);
+    } else if (value === "⨞") {
+      try {
+        let temp = expression.substring(0, expression.length - 1);
+        expression = temp;
+        if (expression) expressionField.innerHTML = expression;
+        else expressionField.innerHTML = 0;
+        console.log(`Expression : ${expression}`);
+      } catch (error) {
+        console.log(error);
+      }
     } else {
       if (!expression) {
         expression = value;
@@ -67,3 +77,36 @@ for (const buttons of btns) {
     }
   });
 }
+
+function updateHistory() {
+  console.log(expressionHistory);
+  const temmpNode=document.createTextNode=`<tr class="expressionValue">
+                                  <th class="expression">Expression</th>
+                                  <th class="exValue">Answer</th>
+                                </tr>`
+  historyContainer.innerHTML=temmpNode
+  console.log(
+    Object.entries(expressionHistory).forEach(([key, value]) => {
+      const exVal = document.createElement("tr");
+      exVal.setAttribute("class", "expressionValue");
+
+      const expre = document.createElement("td");
+      expre.setAttribute("class", "expression");
+      expre.innerHTML = `${key}`;
+
+      const val = document.createElement("td");
+      val.setAttribute("class", "exValue");
+      val.innerHTML = `${value.toFixed(2)}`;
+
+      console.log(expre);
+      console.log(val);
+      exVal.appendChild(expre);
+      exVal.appendChild(val);
+      console.log(exVal);
+
+      historyContainer.appendChild(exVal);
+    })
+  );
+}
+
+
